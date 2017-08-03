9a38556f7747e6142caeb4c854c5187ce583897f
Channa <channa53@gmail.com>
2017-08-03 16:18:53 +0300

    client side

--- server/routes/books.js
+++ server/routes/books.js
@@ -3,6 +3,17 @@ var router = express.Router();
 var Book = require('../models/book');
 
 router.post('/', function (req, res, nex){
-    var book = new Book({});
+    var book = new Book({
+        name: req.body.name,
+        author: req.body.author
+    });
+    book.save(function(err, result){
+        if (err) {
+            return res.status(500).json({
+                title: 'An error occured',
+                error: err
+            });
+        }
+    });
 });
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -28,6 +28,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(logger('dev'));
 app.use(cookieParser());
 
+//the order is important!
 app.use('/book', books);
 app.use('/', index);
 
